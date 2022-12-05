917872d2fed7b0711aad6f45eb793a5bf64a34c8
Artis84 <lopsta10@gmail.com>
2022-12-06 00:59:33 +0100

    Fix a bug

--- scripts/content.js
+++ scripts/content.js
@@ -1,29 +1,47 @@
 console.log("%c[Auto Refresh Stream]", "color: purple", "Waiting for the stat count to show up");
-
 // Watch for changes to the user's delayThreshhold & apply them
-let delayThreshhold;
 chrome.storage.onChanged.addListener((changes, area) => {
+    let newDelayThreshhold;
     if (area === "sync" && changes.delayThreshhold?.newValue) {
-        delayThreshhold = changes.delayThreshhold.newValue.value;
-        console.log("%c[Auto Refresh Stream]\n", "color: purple", "Set delayThreshhold: ", delayThreshhold);
+        newDelayThreshhold = changes.delayThreshhold.newValue.value;
+        console.log("%c[Auto Refresh Stream]\n", "color: purple", "Set delayThreshhold: ", newDelayThreshhold);
     }
 });
 
-// First the function wait for the promise then we set a timeout of 60 seconds so the adblocker i use can block ads whitout the page refreshing because the delay is too high.
+const getDelayThreshhol = async () => {
+    return await chrome.storage.sync.get("delayThreshhold");
+};
+
+const checkStreamDelay = async (delay) => {
+    const $resetButton = document.querySelector(
+        "#root > div > div.Layout-sc-1xcs6mc-0.kBprba > nav > div > div.Layout-sc-1xcs6mc-0.dhONRu > div:nth-child(3) > div > div:nth-child(1) > div.Layout-sc-1xcs6mc-0.kcpvcV > a > div > div.Layout-sc-1xcs6mc-0.hpiroE > div.Layout-sc-1xcs6mc-0.fQKmDn > p"
+    );
+    const delayThreshholValue = {};
+    const data = await getDelayThreshhol();
+    Object.assign(delayThreshholValue, data.delayThreshhold);
+    $resetButton.addEventListener("dblclick", function () {
+        this.style.color = "red";
+        console.log("Mouse Clicked");
+    });
+    if (delay > delayThreshholValue.value) {
+        // console.log("%c[Auto Refresh Stream]", "color: purple", $delay);
+        console.warn("%c[Auto Refresh Stream]", "color: purple", "Refresh the stream");
+        const doubleClickEvent = new Event("dblclick");
+        $resetButton.dispatchEvent(doubleClickEvent);
+    }
+};
+
+// First the function wait for the promise then we set a timeout of 60 seconds so the adblocker
+// can block ads whitout the page refreshing because the delay is too high.
 // Finally i check every 5 seconds if the delay is less than the delaythrehhold.
-waitForElement("button:nth-child(4) > div > span.ffz-stat-text").then((element) => {
+waitForElement(".ffz-stat-text").then((element) => {
     console.log("%c[Auto Refresh Stream]", "color: purple", "the delay count is here!");
     setInterval(() => {
         // console.log("%c[Auto Refresh Stream]", "color: purple", $delay);
-        setTimeout((delay) => {
-            let delayValue = parseFloat(element.textContent);
-            delay = delayValue;
-            if (delay > delayThreshhold) {
-                // console.log("%c[Auto Refresh Stream]", "color: purple", $delay);
-                console.warn("%c[Auto Refresh Stream]", "color: purple", "Refresh the stream");
-                location.reload();
-            }
-        }, 60000);
+        setTimeout(() => {
+            let delay = parseFloat(element.textContent);
+            checkStreamDelay(delay);
+        }, 1000);
     }, 5000);
 });
 
