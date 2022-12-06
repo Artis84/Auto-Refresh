7406bbd80462cb6f73975813c71839c0f1381d91
Artis84 <lopsta10@gmail.com>
2022-12-06 17:03:53 +0100

    Refressh the stream by pause and play intead of refreshing the all page

--- scripts/content.js
+++ scripts/content.js
@@ -1,71 +1,72 @@
-console.log("%c[Auto Refresh Stream]", "color: purple", "Waiting for the stat count to show up");
-// Watch for changes to the user's delayThreshhold & apply them
-chrome.storage.onChanged.addListener((changes, area) => {
-    let newDelayThreshhold;
-    if (area === "sync" && changes.delayThreshhold?.newValue) {
-        newDelayThreshhold = changes.delayThreshhold.newValue.value;
-        console.log("%c[Auto Refresh Stream]\n", "color: purple", "Set delayThreshhold: ", newDelayThreshhold);
-    }
-});
-
-const getDelayThreshhol = async () => {
-    return await chrome.storage.sync.get("delayThreshhold");
-};
-
-const checkStreamDelay = async (delay) => {
-    const $resetButton = document.querySelector(
-        "#root > div > div.Layout-sc-1xcs6mc-0.kBprba > nav > div > div.Layout-sc-1xcs6mc-0.dhONRu > div:nth-child(3) > div > div:nth-child(1) > div.Layout-sc-1xcs6mc-0.kcpvcV > a > div > div.Layout-sc-1xcs6mc-0.hpiroE > div.Layout-sc-1xcs6mc-0.fQKmDn > p"
-    );
-    const delayThreshholValue = {};
-    const data = await getDelayThreshhol();
-    Object.assign(delayThreshholValue, data.delayThreshhold);
-    $resetButton.addEventListener("dblclick", function () {
-        this.style.color = "red";
-        console.log("Mouse Clicked");
-    });
-    if (delay > delayThreshholValue.value) {
-        // console.log("%c[Auto Refresh Stream]", "color: purple", $delay);
-        console.warn("%c[Auto Refresh Stream]", "color: purple", "Refresh the stream");
-        const doubleClickEvent = new Event("dblclick");
-        $resetButton.dispatchEvent(doubleClickEvent);
-    }
-};
-
-// First the function wait for the promise then we set a timeout of 60 seconds so the adblocker
-// can block ads whitout the page refreshing because the delay is too high.
-// Finally i check every 5 seconds if the delay is less than the delaythrehhold.
-waitForElement(".ffz-stat-text").then((element) => {
-    console.log("%c[Auto Refresh Stream]", "color: purple", "the delay count is here!");
-    setInterval(() => {
-        // console.log("%c[Auto Refresh Stream]", "color: purple", $delay);
-        setTimeout(() => {
-            let delay = parseFloat(element.textContent);
-            checkStreamDelay(delay);
-        }, 1000);
-    }, 5000);
-});
-
-/**
- *
- * @param {*} selector
- * @returns A promise containing the element wanted
- */
-function waitForElement(selector) {
-    return new Promise((resolve) => {
-        if (document.querySelector(selector)) {
-            return resolve(document.querySelector(selector));
+chrome.runtime.connect().onDisconnect.addListener(function () {
+    console.log("%c[Auto Refresh Stream]", "color: purple", "Waiting for the stat count to show up");
+    let delay;
+    // Watch for changes to the user's delayThreshhold & apply them
+    chrome.storage.onChanged.addListener((changes, area) => {
+        let newDelayThreshhold;
+        if (area === "sync" && changes.delayThreshhold?.newValue) {
+            newDelayThreshhold = changes.delayThreshhold.newValue.value;
+            console.log("%c[Auto Refresh Stream]\n", "color: purple", "Set delayThreshhold: ", newDelayThreshhold);
         }
-
-        const observer = new MutationObserver((mutations) => {
-            if (document.querySelector(selector)) {
-                resolve(document.querySelector(selector));
-                observer.disconnect();
-            }
-        });
-
-        observer.observe(document.body, {
-            childList: true,
-            subtree: true,
-        });
     });
-}
+
+    const getDelayThreshhol = async () => {
+        return await chrome.storage.sync.get("delayThreshhold");
+    };
+
+    const checkStreamDelay = async () => {
+        const $playButton = document.querySelector(
+            "#channel-player > div > div.Layout-sc-1xcs6mc-0.kEHWUU.player-controls__left-control-group > div.Layout-sc-1xcs6mc-0.ScAttachedTooltipWrapper-sc-1ems1ts-0.deuUPa > button"
+        );
+        const delayThreshholValue = {};
+        const data = await getDelayThreshhol();
+        Object.assign(delayThreshholValue, data.delayThreshhold);
+        $playButton.addEventListener("click", function () {
+            console.log("Click");
+        });
+        if (delay > delayThreshholValue.value) {
+            console.log("%c[Auto Refresh Stream]", "color: purple", "Refresh the stream");
+            $playButton.click();
+            $playButton.click();
+        }
+    };
+
+    // First the function wait for the promise then we set a timeout of 60 seconds so the adblocker
+    // can block ads whitout the page refreshing because the delay is too high.
+    // Finally i check every 5 seconds if the delay is less than the delaythrehhold.
+    waitForElement("button:nth-child(4)").then((element) => {
+        console.log("%c[Auto Refresh Stream]", "color: purple", "the delay count is here!");
+        setInterval(() => {
+            // console.log("%c[Auto Refresh Stream]", "color: purple", $delay);
+            setTimeout(() => {
+                delay = parseInt(element.textContent);
+                checkStreamDelay();
+            }, 60000);
+        }, 5000);
+    });
+
+    /**
+     *
+     * @param {*} selector
+     * @returns A promise containing the element wanted
+     */
+    function waitForElement(selector) {
+        return new Promise((resolve) => {
+            if (document.querySelector(selector)) {
+                return resolve(document.querySelector(selector));
+            }
+
+            const observer = new MutationObserver((mutations) => {
+                if (document.querySelector(selector)) {
+                    resolve(document.querySelector(selector));
+                    observer.disconnect();
+                }
+            });
+
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true,
+            });
+        });
+    }
+});
