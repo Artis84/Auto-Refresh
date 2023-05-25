1289e49210e2aa6886176f3a3ea4af8919d217c6
Artis84 <lopsta10@gmail.com>
2023-05-25 09:53:23 +0200

    minor edit

--- scripts/content.js
+++ scripts/content.js
@@ -24,19 +24,19 @@ document.onreadystatechange = () => {
         // First the function wait for the promise then we set a timeout of 60 seconds so the adblocker
         // can block ads whitout the page refreshing because the delay is too high.
         // Finally i check every 5 seconds if the delay is less than the delaythrehhold.
-        const showDelay = () => {
+        const showMenu = () => {
             console.log("%c[Auto Refresh Stream]", "color: purple", "Initialize extension");
             document
                 .querySelector("#channel-player > div > div.Layout-sc-1xcs6mc-0.lfucH.player-controls__right-control-group > div:nth-child(1) > div:nth-child(2) > div > button > div > div > div")
                 .click();
             document
                 .querySelector(
-                    "#root > div > div.Layout-sc-1xcs6mc-0.kBprba > div > main > div.root-scrollable.scrollable-area.scrollable-area--suppress-scroll-x > div.simplebar-scroll-content > div > div > div.InjectLayout-sc-1i43xsx-0.persistent-player > div > div.Layout-sc-1xcs6mc-0.video-player > div.Layout-sc-1xcs6mc-0.kUDtlR.video-player__container.video-player__container--resize-calc > div.ScReactModalBase-sc-26ijes-0.kXkHnj.tw-dialog-layer.tw-root--theme-dark > div > div > div > div > div > div > div > div > div:nth-child(4) > button"
+                    "body > div.ScReactModalBase-sc-26ijes-0.kXkHnj.tw-dialog-layer.tw-root--theme-dark > div > div > div > div > div > div > div > div:nth-child(2) > div:nth-child(4) > button > div > div.Layout-sc-1xcs6mc-0.fGCgaq"
                 )
                 .click();
             document
                 .querySelector(
-                    "#root > div > div.Layout-sc-1xcs6mc-0.kBprba > div > main > div.root-scrollable.scrollable-area.scrollable-area--suppress-scroll-x > div.simplebar-scroll-content > div > div > div.InjectLayout-sc-1i43xsx-0.persistent-player > div > div.Layout-sc-1xcs6mc-0.video-player > div.Layout-sc-1xcs6mc-0.kUDtlR.video-player__container.video-player__container--resize-calc > div.ScReactModalBase-sc-26ijes-0.kXkHnj.tw-dialog-layer.tw-root--theme-dark > div > div > div > div > div > div > div > div > div:nth-child(6) > div > div > label"
+                    "body > div.ScReactModalBase-sc-26ijes-0.kXkHnj.tw-dialog-layer.tw-root--theme-dark > div > div > div > div > div > div > div > div:nth-child(2) > div:nth-child(3) > div > div > label"
                 )
                 .click();
             document
@@ -47,12 +47,12 @@ document.onreadystatechange = () => {
             ).style.display = "none";
         };
 
-        const getDisplayMenuValue = () => {
+        const hideMenu = () => {
             const attribute = document.createAttribute("id");
             attribute.value = "menu";
             document
                 .querySelector(
-                    "#root > div > div.Layout-sc-1xcs6mc-0.kBprba > div > main > div.root-scrollable.scrollable-area.scrollable-area--suppress-scroll-x > div.simplebar-scroll-content > div > div > div.InjectLayout-sc-1i43xsx-0.persistent-player > div > div.Layout-sc-1xcs6mc-0.video-player > div.Layout-sc-1xcs6mc-0.kUDtlR.video-player__container.video-player__container--resize-calc > div.Layout-sc-1xcs6mc-0.video-ref > div > div > div.tw-root--theme-dark.tw-root--hover > div > div.simplebar-scroll-content"
+                    "#root > div > div.Layout-sc-1xcs6mc-0.kBprba > div > main > div.root-scrollable.scrollable-area.scrollable-area--suppress-scroll-x > div.simplebar-scroll-content > div > div > div.InjectLayout-sc-1i43xsx-0.persistent-player > div > div.Layout-sc-1xcs6mc-0.video-player > div.Layout-sc-1xcs6mc-0.kUDtlR.video-player__container.video-player__container--resize-calc > div.Layout-sc-1xcs6mc-0.video-ref > div > div > div.tw-root--theme-dark.tw-root--hover > div > div.simplebar-scroll-content > div > div"
                 )
                 .setAttributeNode(attribute);
             const $menu = document.getElementById("menu");
@@ -64,12 +64,12 @@ document.onreadystatechange = () => {
         };
 
         const checkStreamDelay = async () => {
-            const options = {};
-            const data = await chrome.storage.sync.get("options");
-            Object.assign(options, data.options);
             const $playButton = document.querySelector(
                 "#channel-player > div > div.Layout-sc-1xcs6mc-0.kEHWUU.player-controls__left-control-group > div.InjectLayout-sc-1i43xsx-0.dVOhMf > button > div > div > div"
             );
+            const options = {};
+            const data = await chrome.storage.sync.get("options");
+            Object.assign(options, data.options);
             const $delay = document.querySelector(
                 "#root > div > div.Layout-sc-1xcs6mc-0.kBprba > div > main > div.root-scrollable.scrollable-area.scrollable-area--suppress-scroll-x > div.simplebar-scroll-content > div > div > div.InjectLayout-sc-1i43xsx-0.persistent-player > div > div.Layout-sc-1xcs6mc-0.video-player > div.Layout-sc-1xcs6mc-0.kUDtlR.video-player__container.video-player__container--resize-calc > div.Layout-sc-1xcs6mc-0.video-ref > div > div > div.tw-root--theme-dark.tw-root--hover > div > div.simplebar-scroll-content > div > div > table > tbody > tr:nth-child(6) > td:nth-child(2)"
             ).textContent;
@@ -110,7 +110,7 @@ document.onreadystatechange = () => {
             delayInterval = 5000;
 
             try {
-                showDelay();
+                showMenu();
             } catch (error) {
                 console.error("%c[Auto Refresh Stream]", "color: purple", "Error during initialization: ", error.message);
                 location.reload();
@@ -119,7 +119,7 @@ document.onreadystatechange = () => {
             setInterval(() => {
                 waitForElm("#channel-player > div > div.Layout-sc-1xcs6mc-0.lfucH.player-controls__right-control-group > div:nth-child(1) > div:nth-child(2) > div > button > div > div > div").then(
                     () => {
-                        getDisplayMenuValue();
+                        hideMenu();
                     }
                 );
             }, 1000);
@@ -128,12 +128,23 @@ document.onreadystatechange = () => {
                 waitForElm(
                     "#root > div > div.Layout-sc-1xcs6mc-0.kBprba > div > main > div.root-scrollable.scrollable-area.scrollable-area--suppress-scroll-x > div.simplebar-scroll-content > div > div > div.InjectLayout-sc-1i43xsx-0.persistent-player > div > div.Layout-sc-1xcs6mc-0.video-player > div.Layout-sc-1xcs6mc-0.kUDtlR.video-player__container.video-player__container--resize-calc > div.Layout-sc-1xcs6mc-0.video-ref > div > div > div.tw-root--theme-dark.tw-root--hover > div > div.simplebar-scroll-content"
                 ).then(() => {
-                    checkStreamDelay();
+                    try {
+                        checkStreamDelay();
+                    } catch (error) {
+                        console.error("%c[Auto Refresh Stream]", "color: purple", "Error during the delay check: ", error.message);
+                        location.reload();
+                    }
                     setTimeout(ticker, delayInterval);
                 });
             })();
 
             console.log("%c[Auto Refresh Stream]", "color: purple", "Done !");
+        });
+        waitForElm(
+            "#root > div > div.Layout-sc-1xcs6mc-0.kBprba > div > main > div.root-scrollable.scrollable-area.scrollable-area--suppress-scroll-x > div.simplebar-scroll-content > div > div > div.InjectLayout-sc-1i43xsx-0.persistent-player > div > div.Layout-sc-1xcs6mc-0.video-player > div.Layout-sc-1xcs6mc-0.kUDtlR.video-player__container.video-player__container--resize-calc > div.Layout-sc-1xcs6mc-0.video-ref > div > div > div.Layout-sc-1xcs6mc-0.fYzbEF > div.Layout-sc-1xcs6mc-0.dHpImr"
+        ).then(() => {
+            console.log("%c[Auto Refresh Stream]", "color: purple", "Reset needed !");
+            location.reload();
         });
     }
 };
